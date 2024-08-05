e9429d5d7154358f0429b410422935d800f3d51f
new user <anoop.r@lenoksolutions.com>
2024-08-05 15:14:20 +0530

    react whatsaappcloud

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{
+      backgroundColor: '#f0f0f0', /* added background color */
+      padding: '20px' /* added padding to create some space around the content */
+    }}>
+  <div style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '20px', marginRight: '20px', paddingTop: '20px' }}>
+    <button>Flight about to depart</button>
+    <button>Boarding started</button>
+  </div>
+  <div style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '20px', marginRight: '20px', paddingTop: '20px' }}>
+    <button>Checkin started</button>
+    <button>Flight delay</button>
+  </div>
+  <div style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '20px', marginRight: '20px', paddingTop: '20px' }}>
+    <button>Safety cone</button>
+    <button>Baggage Found</button>
+  </div>
+  <div style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '20px', marginRight: '20px', paddingTop: '20px' }}>
+    <button>Fire</button>
+    <button>Contract expired</button>
+  </div>
+  <div style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '20px', marginRight: '20px', paddingTop: '20px' }}>
+    <button>Login detected</button>
+    <button>View report</button>
+  </div>
+</div>
   );
 }
 
